dba3e960890adde8035732d37ad02831a4322756
Mitch R <olordhux+github@gmail.com>
2014-07-10 21:27:30 -0700

    stub to handle post requests to the API

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,10 @@ var apiRouter = express.Router();
 
 apiRouter.get('/', function(req, res) {
   res.send('StoryViz JSON API');
+})
+
+.post('/', function(req, res) {
+  req.pipe(res); // for testing
 });
 
 module.exports.apiRouter = apiRouter;
