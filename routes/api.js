cd38c95a5d0328157fa0f6cc2ac4fa833d135c51
Mitch R <olordhux+github@gmail.com>
2014-07-10 23:39:09 -0700

    basic structure for DB integration

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var path    = require('path');
+var express    = require('express');
+var path       = require('path');
 
-var publicDir = require('../server').publicDir;
+var db         = require('../db/db_helpers');
+var publicDir  = require('../server').publicDir;
 
 var apiRouter = express.Router();
 
@@ -12,10 +13,30 @@ apiRouter.get('/', function(req, res) {
 apiRouter.get('/dummy', function(req, res) {
   res.set('Content-Type', 'application/json');
   res.sendfile(path.join(publicDir, 'dummyJSON.json'));
+
+  // Real world will be something like:
+  // var params = req.query
+  // // ... operate on query ...
+  // db.query(params, function(err, data) {
+  //   if(err) { res.send(500,'Internal Server Error'); }
+  //   else {
+  //    res.send(200, JSON.stringify(data));
+  //   }
+  // });
 })
 
 .post('/', function(req, res) {
   req.pipe(res); // for testing
+
+  // Real world will be something like: 
+  // var params = req.body // from bodyparser middleware
+  // ... operate on body ...
+  // db.addNode(params, function(err, data) {
+  //   if(err) { res.send(500,'Internal Server Error'); }
+      // else {
+           // res.send(201, <ID of created user or whatever info we need>);
+      // }
+  // });
 });
 
 module.exports.apiRouter = apiRouter;
