d53f65f18ac15abfa52b82201dc84703aba078d8
Mitch R <olordhux+github@gmail.com>
2014-07-10 21:51:14 -0700

    serve static files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var indexRouter = express.Router();
 
 indexRouter.get('/', function(req, res) {
-  res.send('StoryViz');
+
 });
 
 module.exports.indexRouter = indexRouter;
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 var express = require('express');
-var logger = require('morgan'); // Logging middleware
+var path    = require('path');
+var logger  = require('morgan'); // Logging middleware
 
 var app     = express();
 var port    = process.env.PORT || 8000;
 
-// Routes
-
-
 // Middlewares
 app.use(logger()); // Morgan
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', require('./routes/index').indexRouter);
+// Routes
+// app.use('/', require('./routes/index').indexRouter);
 app.use('/api', require('./routes/api').apiRouter);
-
 
 app.listen(port);
 console.log('Server running on', port);
