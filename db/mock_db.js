9299d64a25529f40dc212401dd9d474d9d596fa6
Mitch R <olordhux+github@gmail.com>
2014-07-11 17:20:44 -0700

    DB clearing and mocking, for testing.

--- db/mock_db.js
+++ db/mock_db.js
@@ -1,12 +1,15 @@
 var neo4j = require('neo4j');
 var path  = require('path');
 
-var db = new neo4j.GraphDatabase('http://localhost:7474');
+var db         = new neo4j.GraphDatabase('http://localhost:7474');
+var api        = require('./api_helpers');
 var publicDir  = require('../helpers/path_helpers').publicDir;
+var dummyJSON  = require(path.join(publicDir, 'dummyJSON.json'));
 
+/**
+ * Clear the DB, then fill it with users from dummyJSON.json
+ */
 function mockDB() {
-  // var dummyJSON = require(path.join(publicDir, 'dummyJSON.json'));
-
   var clearDb = [
   'MATCH (n)',
   'OPTIONAL MATCH (n)-[r]-()',
@@ -16,16 +19,18 @@ function mockDB() {
   // clear the DB
   db.query(clearDb,{}, function(){});
   console.log('DB cleared.');
-  // console.log('adding fake data to db...');
-  
-  // dummyJSON.nodes.forEach(function(node) {
-  //   db.createNode({name: node.name}).save(function(err, node){
 
-  //   });
-
-  // });
+  console.log('adding fake data to db...');
+  dummyJSON.nodes.forEach(function(node) {
+    api.saveNewCharacter({name: node.name}, function(err, id) {
+      if(err) { console.log(err); }
+      else {
+        console.log('New mock user created: ' + id);
+      }
+    });
+  });
   // dummyJSON.links.forEach(function(link) {
-  //   // db.createRelationshipFrom();
+  //   db.createRelationshipFrom();
   // });
 }
 
