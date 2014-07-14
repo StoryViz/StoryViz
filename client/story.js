a52c04746fec29f2ff49d2c1467b6834ac4591fa
valiantvu <michelle.thi.vu@gmail.com>
2014-07-14 11:57:53 -0700

    Add methods to $scope for getAll, setChar, and setRelationships in Story Controller

--- client/story.js
+++ client/story.js
@@ -1,6 +1,8 @@
 angular.module('storyviz.story', [])
   .controller('StoryController', function($scope, Story) {
       $scope.data = {};
+
+      // gets dummy data
       $scope.getAllChars = function() {
         Story.getAllChars()
           .then(function(data) {
@@ -13,4 +15,48 @@ angular.module('storyviz.story', [])
           });
       };
       $scope.getAllChars();
+
+      // get all names and relationships
+      $scope.getAll = function() {
+        Story.getAll()
+          .then(function(data) {
+            //data is an object with node and link properties
+            $scope.data = data;
+          })
+          .catch(function(err) {
+            console.log(err);
+            throw err;
+          });
+      };
+
+      $scope.getAll();
+
+      // setChar will be called from view (e.g. on click)
+      $scope.setChar = function() {
+        // $scope.name should be set through data binding 
+        // in view (e.g. input field)
+        Story.setChar($scope.name)
+          .then(function(response) {
+            console.log("response from setChar: ", response);
+          })
+          .catch(function(err) {
+            console.log(err);
+            throw err;
+          });
+      };
+
+      $scope.setRelationship = function() {
+        // $scope.from, $scope.to, and $scope.type
+         // should be set through data binding 
+        // in view (e.g. input field)
+        Story.setRelationship($scope.from, $scope.to, $scope.type)
+          .then(function(response) {
+            console.log("response from setRelationship: ", response);
+          })
+          .catch(function(err) {
+            console.log(err);
+            throw err;
+          });
+      };
+      
   });
