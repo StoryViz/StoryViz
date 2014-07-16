4a40d12f8b1047c8bbdf76d3b48e863ad2e77f0a
valiantvu <michelle.thi.vu@gmail.com>
2014-07-16 09:57:55 -0700

    Add scope variable for relationship types, adjust indentation

--- client/story.js
+++ client/story.js
@@ -1,91 +1,92 @@
 angular.module('storyviz.story', [])
   .controller('StoryController', function($scope, Story) {
-      $scope.data = {};
+    $scope.relationshipTypes = ['Family', 'Near', 'Allies', 'Enemies', 'Kills', 'Romance'];
+    $scope.data = {};
 
-      // stores all relationships for each character
-      // enables isolation of a single character and their
-      // relationships
-      $scope.characterRelationships = {};
+    // stores all relationships for each character
+    // enables isolation of a single character and their
+    // relationships
+    $scope.characterRelationships = {};
 
-      // newCharacter stores node data for character being added
-      $scope.newCharacter = {};
+    // newCharacter stores node data for character being added
+    $scope.newCharacter = {};
 
-      // newRelationship stores link data for relationship being added
-      $scope.newRelationship = {};
+    // newRelationship stores link data for relationship being added
+    $scope.newRelationship = {};
 
-      // character stores node data for selected character
-      $scope.selectedCharacter = {};
+    // character stores node data for selected character
+    $scope.selectedCharacter = {};
 
-      // Get all characters and relationships
-      $scope.getAll = function() {
-        Story.getAll()
-          .then(function(data) {
-            var nodes = data.data.nodes;
-            var nodeIndexStorage = {};
-            var links = data.data.links;
-            var linkStorage = [];
+    // Get all characters and relationships
+    $scope.getAll = function() {
+      Story.getAll()
+        .then(function(data) {
+          var nodes = data.data.nodes;
+          var nodeIndexStorage = {};
+          var links = data.data.links;
+          var linkStorage = [];
 
-            // Save array index of each node in nodeIndexStorage object
-            for (var i = 0; i < nodes.length; i++) {
-              nodeIndexStorage[nodes[i].id] = i;
-            }
+          // Save array index of each node in nodeIndexStorage object
+          for (var i = 0; i < nodes.length; i++) {
+            nodeIndexStorage[nodes[i].id] = i;
+          }
 
-            for (var j = 0; j < links.length; j++) {
-              var newLink = {};
+          for (var j = 0; j < links.length; j++) {
+            var newLink = {};
 
-              // Change source to refer to array index instead of character id
-              var charIDSource = links[j].source;
-              var charIDTarget = links[j].target;
-              newLink.source = nodeIndexStorage[charIDSource];
-              newLink.target = nodeIndexStorage[charIDTarget];
-              newLink.type = links[j].type;
-              linkStorage.push(newLink);
-            }
+            // Change source to refer to array index instead of character id
+            var charIDSource = links[j].source;
+            var charIDTarget = links[j].target;
+            newLink.source = nodeIndexStorage[charIDSource];
+            newLink.target = nodeIndexStorage[charIDTarget];
+            newLink.type = links[j].type;
+            linkStorage.push(newLink);
+          }
 
-            $scope.data = {nodes: nodes, links: linkStorage};
-          })
-          .catch(function(err) {
-            console.log(err);
-            throw err;
-          });
-      };
+          $scope.data = {nodes: nodes, links: linkStorage};
+        })
+        .catch(function(err) {
+          console.log(err);
+          throw err;
+        });
+    };
 
-      $scope.getAll();
+    $scope.getAll();
 
-      $scope.getCharacterRelationships = function() {
-        // $scope.selectedCharacter.id;
-      };
+    $scope.getCharacterRelationships = function() {
+      // $scope.selectedCharacter.id;
+    };
 
-      // Add new character
-      // addChar called from view on click
-      $scope.addChar = function() {
-        // $scope.name should be set through data binding 
-        // in view (e.g. input field)
-        Story.addChar($scope.newCharacter.name)
-          .then(function(response) {
+    // Add new character
+    // addChar called from view on click
+    $scope.addChar = function() {
+      // $scope.name should be set through data binding 
+      // in view (e.g. input field)
+      Story.addChar($scope.newCharacter.name)
+        .then(function(response) {
 
-            // concat used instead of push in order to trigger change
-            $scope.data.nodes = $scope.data.nodes.concat(response.data);
-          })
-          .catch(function(err) {
-            console.log(err);
-            throw err;
-          });
-      };
+          // concat used instead of push in order to trigger change
+          $scope.data.nodes = $scope.data.nodes.concat(response.data);
+        })
+        .catch(function(err) {
+          console.log(err);
+          throw err;
+        });
+    };
 
-      // Add new relationship
-      $scope.addRel = function() {
-        // $scope.from, $scope.to, and $scope.type
-         // should be set through data binding 
-        // in view (e.g. input field)
+    // Add new relationship
+    $scope.addRel = function() {
+      // $scope.from, $scope.to, and $scope.type
+       // should be set through data binding 
+      // in view (e.g. input field)
 /*        Story.addRel($scope.from, $scope.to, $scope.type)
-          .then(function(response) {
-            console.log("response from addRelationship: ", response);
-          })
-          .catch(function(err) {
-            console.log(err);
-            throw err;
-          });*/
-      };
-      console.log('addRel invoked from story controller');
+        .then(function(response) {
+          console.log("response from addRelationship: ", response);
+        })
+        .catch(function(err) {
+          console.log(err);
+          throw err;
+        });*/
+    };
+    console.log('addRel invoked from story controller');
   });
